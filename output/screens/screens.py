73f73cbba272b8cf0ed546ead010dcfb513c66a0
Matt Crow <greengrappler12@gmail.com>
2021-06-26 15:42:38 -0700

    I'm not feeling up to programming now

--- output/screens/screens.py
+++ output/screens/screens.py
@@ -24,10 +24,34 @@ class Screen:
 
     def add(self, x, y, msg):
         msg = str(msg)
+
+        lines = msg.split("\n")
+
+        for line in lines:
+            endOfWord = line.find(' ', SCREEN_COLS - x - 1)
+            if endOfWord == -1:
+                endOfWord = len(line)
+            while endOfWord != -1: # too wide to fit on one row
+                # write across multiple rows
+                self.addLine(x, y, line[:endOfWord])
+                y = y + 1
+                line = line[endOfWord + 1:]
+                endOfWord = line.find(' ', SCREEN_COLS - x - 1)
+            # by now, the line is narrow enough to fit
+            if len(line) != 0:
+                self.addLine(x, y, line)
+                y = y + 1
+
+    def addLine(self, x, y, msg):
+        print(msg)
+        msg = str(msg)
         length = len(msg)
-        numChars = length if length + x < SCREEN_COLS else SCREEN_COLS - x
+        numChars = length if length + x - 1 < SCREEN_COLS else SCREEN_COLS - x - 1
         for i in range(numChars):
-            self.content[y][x + i] = msg[i]
+            if msg[i] == '\n':
+                y = y + 1
+            else:
+                self.content[y][x + i] = msg[i]
 
     def display(self):
         for row in self.content:
