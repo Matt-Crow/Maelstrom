fa2b1bcc374d54565952d72e5860947ba52f11a6
Matt Crow <greengrappler12@gmail.com>
2021-06-29 16:08:24 -0700

    Just threw that idea together, as I imagine it will be helpful

---
+++ output/output.py
@@ -0,0 +1,42 @@
+"""
+This file details which functions will receive program output and debug
+information
+
+Primary exports:
+* print(msg)
+* debug(msg)
+"""
+
+DEBUG = false
+
+class OutputConsumer:
+    def print(self, msg):
+        raise NotImplementedError("OutputConsumer::print is an abstract method")
+
+class TerminalLogger(OutputConsumer):
+    def print(self, msg):
+        print(msg);
+
+"""
+class FileLogger(OutputConsumer):
+"""
+
+class DevNull(OutputConsumer):
+    def print(self, msg):
+        pass
+
+
+outputConsumers = [TerminalLogger()]
+debugConsumers = []
+if DEBUG:
+    debugConsumers.append(TerminalLogger())
+else:
+    debugConsumers.append(DevNull())
+
+def print(msg):
+    for consumer in outputConsumers:
+        consumer.print(msg)
+
+def debug(msg):
+    for consumer in debugConsumers:
+        consumer.print(msg)
